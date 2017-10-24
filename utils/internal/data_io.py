edac9cadf41ab88f603e3f348f4f6177fa891359
Derek Chen <derekchen14@gmail.com>
2017-10-24 14:42:56 -0700

    Updated data io

--- utils/internal/data_io.py
+++ utils/internal/data_io.py
@@ -3,7 +3,10 @@ Ref: https://github.com/llSourcell/How_to_make_a_chatbot/blob/master/memorynetwo
 Most of them are helper functions. You may only read and use get_word(), read_from_file(), and
 load_glove()
 '''
-import torchtext.vocab as vocab
+import random
+import numpy as np
+from scipy.stats import truncnorm
+import math
 
 def tokenize(sent):
     '''Return the tokens of a sentence including punctuation.
@@ -35,41 +38,62 @@ def parse_dialogue(lines, only_supporting=False):
     return data
 
 
-def get_dialogue(f, only_supporting=False, max_length=None):
+def word_to_glove_vector(glove, word):
     '''
-    f: open(file) object
-    returns [[(u1, r1), (u2, r2)...], [(u1, r1), (u2, r2)...], ...]
-    '''
-    data = parse_dialogue(f.readlines(), only_supporting=only_supporting)
-    return data
-
-
-def get_word(glove, word):
-    '''
-    :param glove: Glove object from pytorchtext
+    :param embeddinds: Glove object from pytorchtext
     :param word: str
     :return: the embedding vector of the word
     '''
     return glove.vectors[glove.stoi[word]]
 
 
-def read_from_file(filename):
+def read_restuarant_data(filename):
     '''
     :param filename: 'directory/file.txt'
-    :return:[[(u1, r1), (u2, r2)...], [(u1, r1), (u2, r2)...], ...]
+    :return:[
+        [(u1, r1), (u2, r2)...]
+        , [(u1, r1), (u2, r2)...], ...]
+    the data is a list of training examples
+    each example consists of one dialog
+    for each dialog, there are a number of turns
+    each turn is made up of a tuple of (u_i, r_i) for up to N turns
+        where ui is utterance from the customer
+        where ri is a response from an agent
+    each ui or ri, is a list of strings, for up to M tokens
+        each token is usually a word or punctuation
+    if the customer said nothing during their turn,
+        special token of <SILENCE> is used
     '''
-    f = open(filename)
-    data = get_dialogue(f)
+    with open(filename) as f:
+        # max_length = None
+        data = parse_dialogue(f.readlines(), only_supporting=False)
     return data
 
-def load_glove(name='6B', dim=100):
+def read_car_data(filename):
+    pass
+
+
+def init_glove_words(name='6B', dim=100):
     '''
     :param name: which glove you want to load
     :param dim: dimension of word vectors
     :return: the glove object in pytorch
     '''
+    import torchtext.vocab as vocab
     glove = vocab.GloVe(name='6B', dim=100)
     print('Loaded {} words'.format(len(glove.itos)))
-    return glove
+    word_embeddings = glove
+    return word_embeddings
+
+def init_normal_words(vocab_size=1229, dim=100):
+    # add EOS and SOS and UNK?
+    mean = 0
+    stddev = 1.0/math.sqrt(2 * math.pi)
+    # each word embedding is a column vector
+    word_embeddings = truncnorm.rvs(a=mean, b=stddev, size=[dim, vocab_size])
+    return word_embeddings
+
+
+
 
 
