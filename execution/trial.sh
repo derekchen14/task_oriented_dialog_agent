371620a4adf4c6c1f67016875761f535f6087043
Ubuntu <alexiaxu@alexia2.pbpjetp5bvwuvj4ix1bitp0x0a.xx.internal.cloudapp.net>
2017-12-02 01:40:23 +0000

    More exp

--- execution/trial.sh
+++ execution/trial.sh
@@ -4,10 +4,10 @@
 
 # python 4_attention.py --task-name dstc --hidden-size 512 --n-layers 1 --drop-prob 0.0 -enp 'results/1layer_en.pt'
 # -edp 'results/1layer_de.pt' -ep 'results/1layer_error.csv' -s
-~                                                      
+# ~                                                      
 # python 4_attention.py --task-name dstc --hidden-size 256 --n-layers 3 --drop-prob 0.0 -enp 'results/3layer_en.pt'
 # -edp 'results/3layer_de.pt' -ep 'results/3layer_error.csv' -s
-~
+#~
 
 #-----------seq2seq -> attention -> teacher forcing---------------
 # seq2seq no lr decay
