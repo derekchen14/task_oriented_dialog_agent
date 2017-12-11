cf8cc39838b9b609a3fcc00bfdcca3bf2171d9c8
Alexia Xu <alexiaxu@Alexias-MacBook-Pro.local>
2017-12-10 22:03:27 -0800

    bid tiral

--- execution/bid.sh
+++ execution/bid.sh
@@ -9,3 +9,9 @@ python 4_attention.py -t challenge --hidden-size 512 --n-layers 1 --drop-prob 0.
 
 python 4_attention.py -t challenge --hidden-size 512 --n-layers 1 --drop-prob 0.5 -ep 'results/bid5_error.csv' -teach 0.60
 
+python 4_attention.py -t challenge --hidden-size 512 --n-layers 1 --drop-prob 0.75 -ep 'results/bid6_error.csv' -teach 0.60
+
+python 4_attention.py -t challenge --hidden-size 512 --n-layers 1 --drop-prob 0.95 -ep 'results/bid7_error.csv' -teach 0.60
+
+
+
