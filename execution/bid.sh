f95c56d803b796c6d102ddfb1e7aaf265061c46b
Alexia Xu <alexiaxu@Alexias-MacBook-Pro.local>
2017-12-12 19:35:54 -0800

    .

--- execution/bid.sh
+++ execution/bid.sh
@@ -13,9 +13,12 @@
 
 #python 4_attention.py -t challenge --hidden-size 512 --n-layers 1 --drop-prob 0.95 -ep 'results/bid7_error.csv' -teach 0.60
 
-python 4_attention.py -t challenge --hidden-size 512 --n-layers 1 --drop-prob 0.3 -enp 'results/bid_best.pt' -edp 'results/bid_best.pt' -ep 'results/bidre3_error.csv' -teach 0.60
+#python 4_attention.py -t challenge --hidden-size 512 --n-layers 1 --drop-prob 0.3 -ep 'results/bidre3_error.csv' -teach 0.60
 
 #python 4_attention.py -t challenge --hidden-size 512 --n-layers 1 --drop-prob 0.5 -ep 'results/bidre5_error.csv' -teach 0.60
+
+python 4_attention.py -t challenge --hidden-size 256 --n-layers 1 --drop-prob 0.3 -enp 'results/bid_best.pt' -edp 'results/bid_best.pt' -ep 'results/bid_best_error.csv' -teach 0.60
+
 
 
 
