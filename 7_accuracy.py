56a95ba9ac996e27af90cd9fb8ac449bc47f5931
Derek Chen <derekchen14@gmail.com>
2017-12-13 09:32:45 -0800

    First draft at calculating accuracy scores

--- 7_accuracy.py
+++ 7_accuracy.py
@@ -9,21 +9,14 @@ import random
 import json
 import sys
 import time as tm
-import pandas as pd
 
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
-from torch import optim
-from torch.optim.lr_scheduler import StepLR as StepLR
 
 import utils.internal.data_io as data_io
-import utils.internal.display as display
 from utils.external.clock import *
 from utils.external.preprocessers import *
-
-from model.encoders import GRU_Encoder #GRU_Encoder # LSTM_Encoder, RNN_Encoder
-from model.decoders import GRU_Attn_Decoder # GRU_Attn_Decoder # RNN_Attn_Decoder
 
 use_cuda = torch.cuda.is_available()
 MAX_LENGTH = 8
@@ -35,7 +28,6 @@ def evaluate(input_variable, target_variable, encoder, decoder, max_length):
   target_length = target_variable.size()[0]
   encoder_outputs = Variable(torch.zeros(max_length, encoder.hidden_size))
   encoder_outputs = encoder_outputs.cuda() if use_cuda else encoder_outputs
-  loss = 0
 
   for ei in range(min(max_length, input_length)):
     encoder_output, encoder_hidden = encoder(input_variable[ei], encoder_hidden)
@@ -43,7 +35,6 @@ def evaluate(input_variable, target_variable, encoder, decoder, max_length):
 
   decoder_input = Variable(torch.LongTensor([[vocab.SOS_token]]))
   decoder_input = decoder_input.cuda() if use_cuda else decoder_input
-  # encoder's last hidden state is the decoder's intial hidden state
   decoder_hidden = encoder_hidden
 
   predicts = []
@@ -57,42 +48,41 @@ def evaluate(input_variable, target_variable, encoder, decoder, max_length):
     ni = topi[0][0]
     decoder_input = Variable(torch.LongTensor([[ni]]))
     decoder_input = decoder_input.cuda() if use_cuda else decoder_input
-
     predicts.append(ni)
-    # decoder_output = predicted output distribution (1x2165) Float Tensor
-    # target_variable[di] = correct word? Long Tensor (scalar 1)
 
-  target = target_variable.data.tolist()
-  accuracy = 0
-  for pred, tar in zip(predicts, targets):
-    print(pred, tar)
-    print(pred == tar)
-    if pred == tar:
-      accuracy += 1
-  sys.exit()
-
-  turn_success = accuracy == (target_length)
-  return turn_success
+  targets = target_variable.data.tolist()
+  turn_success = [pred == tar for pred, tar in zip(predicts, targets)]
+  return all(turn_success)
 
 if __name__ == "__main__":
-  # ---- PARSE ARGS -----
   args = solicit_args()
   task = 'car' if args.task_name in ['navigate', 'schedule', 'weather'] else 'res'
-  # ----- LOAD DATA -----
   val_data, val_candidates, max_length = data_io.load_dataset(args.task_name, "dev", False)
-  val_variables = collect_dialogues(val_data, task=task)[0:1000]
-  # ---- BUILD MODEL ----
+  val_variables = collect_dialogues(val_data, task=task)[0:10]
   encoder = torch.load('datasets/old_encoder.pt')
   decoder = torch.load('datasets/old_decoder.pt')
-  # ------ EVALUATE ------
-  per_turn_success = []
-  per_dialog_success = []
-  for pair in val_variables:
-     turn_success = evaluate(pair[0], pair[1], encoder, decoder, max_length)
-     per_turn_accuracy.append(turn_success)
-     # if new_dialog:
-     #    per_dialog_success.append(acc_so_far)
 
-  per_turn_accuracy = float(per_turn_success) / len(val_variables)
-  per_dialog_accuracy = float(per_dialog_success) / 300
-  print round(per_turn_accuracy, 3)
+  # single_dialog_success and single_turn_success are scalars with values 0 or 1
+  # dialog_success and turn_success are lists that hold "single success"
+  # overall_dialog and overall_turn are lists that hold accuracy totals
+  # per_dialog_accuracy and per_turn_accuracy are floats between 0% to 100%
+  overall_dialog = []
+  overall_turn = []
+  dialog_success = None
+
+  for input_var, output_var in val_variables:
+    first_token = input_var[0].data[0]
+    # first token is a turn counter, so when it equals 1, that means we are in a new dialogue
+    if first_token == 1 and dialog_success:
+      single_dialog_success = all(dialog_success)
+      overall_dialog.append(single_dialog_success)
+      dialog_success = []
+
+    single_turn_success = evaluate(input_var, output_var, encoder, decoder, max_length)
+    dialog_success.append(single_turn_success)
+    overall_turn.append(single_turn_success)
+
+  per_turn_accuracy = float(sum(overall_turn)) / len(overall_turn)
+  print("per_turn_accuracy: {}".format(round(per_turn_accuracy, 3)) )
+  per_dialog_accuracy = float(sum(overall_dialog)) / len(overall_dialog)
+  print("per_dialog_accuracy: {}".format(round(per_dialog_accuracy, 3)) )
