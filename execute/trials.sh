26db2702bf49825ca5e7122810fe0e2493ae663d
Derek Chen <derekchen14@gmail.com>
2018-02-08 22:48:18 -0800

    update the execution scripts for new results reporting

--- execute/trials.sh
+++ execute/trials.sh
@@ -1,7 +1,1 @@
-python 4_attention.py -t challenge --save-loss --loss-path results/half_drop1.csv --drop-prob 0.5
-python 4_attention.py -t challenge --save-loss --loss-path results/half_drop2.csv --drop-prob 0.5
-python 4_attention.py -t challenge --save-loss --loss-path results/half_drop3.csv --drop-prob 0.5
-
-python 4_attention.py -t challenge --save-loss --loss-path results/low_drop1.csv --drop-prob 0.2
-python 4_attention.py -t challenge --save-loss --loss-path results/low_drop2.csv --drop-prob 0.2
-python 4_attention.py -t challenge --save-loss --loss-path results/low_drop3.csv --drop-prob 0.2
+python 4_attention.py -t challenge --report-results --results-path results/new_baseline.csv
