861f8b673f3c7cabe2f628d59b440e2abf12f0d7
Derek Chen <derekchen14@gmail.com>
2018-02-11 21:41:36 -0800

    Clean house and remove unneeded files

--- execute/opt.sh
+++
@@ -1,4 +0,0 @@
-python 4_attention.py -t challenge --hidden-size 256 --n-layers 1 --drop-prob 0.0 -op 'Adam' -ep 'results/adam_error.csv' -teach 0.60
-python 4_attention.py -t challenge --hidden-size 256 --n-layers 1 --drop-prob 0.0 -op 'RMSprop' -ep 'results/rms_error.csv' -teach 0.60
-
-
