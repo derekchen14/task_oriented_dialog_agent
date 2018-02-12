e47041d060b467b5b2fd56befa75fd4ccd39cdfe
Derek Chen <derekchen14@gmail.com>
2018-02-11 16:12:16 -0800

    Another typo with execution

--- execute/attention.sh
+++ execute/attention.sh
@@ -2,24 +2,24 @@
 #     --drop-prob 0.0 -enp 'results/seq_en.pt' -edp 'results/seq_de.pt' \
 #     -ep 'results/seq_error.csv' -s -teach 0.0
 
-python 4_attention.py --task-name challenge --attn_method luong \
+python 4_attention.py --task-name challenge --attn-method luong \
     --report-results --results-path results/luong1.csv
-python 4_attention.py --task-name challenge --attn_method luong \
+python 4_attention.py --task-name challenge --attn-method luong \
     --report-results --results-path results/luong2.csv
-python 4_attention.py --task-name challenge --attn_method luong \
+python 4_attention.py --task-name challenge --attn-method luong \
     --report-results --results-path results/luong3.csv
 
-python 4_attention.py --task-name challenge --attn_method vinyals \
+python 4_attention.py --task-name challenge --attn-method vinyals \
     --report-results --results-path results/vinyals1.csv
-python 4_attention.py --task-name challenge --attn_method vinyals \
+python 4_attention.py --task-name challenge --attn-method vinyals \
     --report-results --results-path results/vinyals2.csv
-python 4_attention.py --task-name challenge --attn_method vinyals \
+python 4_attention.py --task-name challenge --attn-method vinyals \
     --report-results --results-path results/vinyals3.csv
 
-python 4_attention.py --task-name challenge --attn_method dot \
+python 4_attention.py --task-name challenge --attn-method dot \
     --report-results --results-path results/dot1.csv
-python 4_attention.py --task-name challenge --attn_method dot \
+python 4_attention.py --task-name challenge --attn-method dot \
     --report-results --results-path results/dot2.csv
-python 4_attention.py --task-name challenge --attn_method dot \
+python 4_attention.py --task-name challenge --attn-method dot \
     --report-results --results-path results/dot3.csv
 
