f3fc2b170392c6458ce3730108e9a3013ad5a575
Derek Chen <derekchen14@gmail.com>
2018-02-24 15:15:54 -0800

    Fix typo with adam, new set of trials

--- execute/trials.sh
+++ execute/trials.sh
@@ -1,23 +1,26 @@
 python 3_advanced.py --task-name challenge --model-type copy \
-    --report-results --results-path results/copy1a.csv \
-    --weight-decay 0.0 --drop-prob 0.0
-python 3_advanced.py --task-name challenge --model-type copy \
-    --report-results --results-path results/copy1b.csv \
-    --weight-decay 0.0 --drop-prob 0.0
-
-python 3_advanced.py --task-name challenge --model-type copy \
-    --report-results --results-path results/copy2a.csv
-python 3_advanced.py --task-name challenge --model-type copy \
-    --report-results --results-path results/copy2b.csv
-
-python 3_advanced.py --task-name challenge --model-type copy \
-    --report-results --results-path results/copy3a.csv \
-    --teacher-forcing 0.8
-python 3_advanced.py --task-name challenge --model-type copy \
-    --report-results --results-path results/copy3b.csv \
-    --teacher-forcing 0.8
-
-python 3_advanced.py --task-name challenge --model-type copy \
     --report-results --results-path results/copy4a.csv --optimizer Adam
 python 3_advanced.py --task-name challenge --model-type copy \
     --report-results --results-path results/copy4b.csv --optimizer Adam
+
+python 3_advanced.py --task-name challenge --model-type copy \
+    --report-results --results-path results/copy5a.csv \
+    --weight-decay 0.0 --drop-prob 0.1
+python 3_advanced.py --task-name challenge --model-type copy \
+    --report-results --results-path results/copy5b.csv \
+    --weight-decay 0.0 --drop-prob 0.1
+
+python 3_advanced.py --task-name challenge --model-type copy \
+    --report-results --results-path results/copy6a.csv \
+    --n-iters 30000 --decay-times 4 --attn-method vinyals
+python 3_advanced.py --task-name challenge --model-type copy \
+    --report-results --results-path results/copy6b.csv \
+    --n-iters 30000 --decay-times 4 --attn-method vinyals
+
+python 3_advanced.py --task-name challenge --model-type copy \
+    --report-results --results-path results/copy7a.csv \
+    --n-iters 30000 --decay-times 4 --attn-method luong
+python 3_advanced.py --task-name challenge --model-type copy \
+    --report-results --results-path results/copy7b.csv \
+    --n-iters 30000 --decay-times 4 --attn-method luong
+
