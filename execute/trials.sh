8ce089304a320caae9efc691fe4b1e97a29532d4
Derek Chen <derekchen14@gmail.com>
2018-02-23 20:33:21 -0800

    Update tuning parameter options

--- execute/trials.sh
+++ execute/trials.sh
@@ -1,13 +1,23 @@
-python 4_attention.py --task-name challenge --model-type attention \
-    --report-results --results-path results/attn1.csv
-python 4_attention.py --task-name challenge --model-type attention \
-    --report-results --results-path results/attn2.csv
-python 4_attention.py --task-name challenge --model-type attention \
-    --report-results --results-path results/attn3.csv
+python 3_advanced.py --task-name challenge --model-type copy \
+    --report-results --results-path results/copy1a.csv \
+    --weight-decay 0.0 --drop-prob 0.0
+python 3_advanced.py --task-name challenge --model-type copy \
+    --report-results --results-path results/copy1b.csv \
+    --weight-decay 0.0 --drop-prob 0.0
 
-python 4_attention.py --task-name challenge --model-type match \
-    --report-results --results-path results/match1.csv
-python 4_attention.py --task-name challenge --model-type match \
-    --report-results --results-path results/match2.csv
-python 4_attention.py --task-name challenge --model-type match \
-    --report-results --results-path results/match3.csv
+python 3_advanced.py --task-name challenge --model-type copy \
+    --report-results --results-path results/copy2a.csv
+python 3_advanced.py --task-name challenge --model-type copy \
+    --report-results --results-path results/copy2b.csv
+
+python 3_advanced.py --task-name challenge --model-type copy \
+    --report-results --results-path results/copy3a.csv \
+    --teacher-forcing 0.8
+python 3_advanced.py --task-name challenge --model-type copy \
+    --report-results --results-path results/copy3b.csv \
+    --teacher-forcing 0.8
+
+python 3_advanced.py --task-name challenge --model-type copy \
+    --report-results --results-path results/copy4a.csv --optimizer Adam
+python 3_advanced.py --task-name challenge --model-type copy \
+    --report-results --results-path results/copy4b.csv --optimizer Adam
