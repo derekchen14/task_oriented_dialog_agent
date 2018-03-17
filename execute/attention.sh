1297a32dc7945bbd9b427f839665ee32deb18297
Derek Chen <derekchen14@gmail.com>
2018-03-17 10:40:30 -0700

    First full run of tranformer

--- execute/attention.sh
+++ execute/attention.sh
@@ -1,21 +1,17 @@
-python 4_attention.py --task-name challenge --attn-method luong \
-    --report-results --results-path results/luong1.csv
-python 4_attention.py --task-name challenge --attn-method luong \
-    --report-results --results-path results/luong2.csv
-python 4_attention.py --task-name challenge --attn-method luong \
-    --report-results --results-path results/luong3.csv
+python 3_advanced.py --task-name challenge --model-type transformer \
+    --optimizer Adam --n-layers 6 --n-iters 30000 --save-model \
+    --encoder-path results/enc_poster1a.pt  --encoder-path results/dec_poster1a.pt \
+    --report-results --results-path results/poster1a.csv -lr 0.0158
+python 3_advanced.py --task-name challenge --model-type transformer \
+    --optimizer Adam --n-layers 6 --n-iters 30000 --save-model \
+    --encoder-path results/enc_poster1b.pt  --encoder-path results/dec_poster1b.pt \
+    --report-results --results-path results/poster1b.csv -lr 0.0158
 
-python 4_attention.py --task-name challenge --attn-method vinyals \
-    --report-results --results-path results/vinyals1.csv
-python 4_attention.py --task-name challenge --attn-method vinyals \
-    --report-results --results-path results/vinyals2.csv
-python 4_attention.py --task-name challenge --attn-method vinyals \
-    --report-results --results-path results/vinyals3.csv
-
-python 4_attention.py --task-name challenge --attn-method dot \
-    --report-results --results-path results/dot1.csv
-python 4_attention.py --task-name challenge --attn-method dot \
-    --report-results --results-path results/dot2.csv
-python 4_attention.py --task-name challenge --attn-method dot \
-    --report-results --results-path results/dot3.csv
-
+python 3_advanced.py --task-name challenge --model-type transformer \
+    --optimizer Adam --n-layers 6 --n-iters 30000 --save-model \
+    --encoder-path results/enc_poster2a.pt  --encoder-path results/dec_poster2a.pt \
+    --report-results --results-path results/poster2a.csv -lr 0.001
+python 3_advanced.py --task-name challenge --model-type transformer \
+    --optimizer Adam --n-layers 6 --n-iters 30000 --save-model \
+    --encoder-path results/enc_poster2b.pt  --encoder-path results/dec_poster2b.pt \
+    --report-results --results-path results/poster2b.csv -lr 0.001
