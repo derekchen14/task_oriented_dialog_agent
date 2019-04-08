607d27cf15dc43c4912354680bcbe3404177497f
Derek Chen <derekchen14@gmail.com>
2019-04-08 10:33:34 -0700

    Clean up track intent bash script to incorporate the glad early stop metric

--- execute/track_intent.sh
+++ execute/track_intent.sh
@@ -1,10 +1,4 @@
-CUDA_VISIBLE_DEVICES=1  python run.py --task track_intent --dataset woz2 --model glad \
-      --learning-rate 1e-3 --hidden-dim 200 --embedding-size 400 --epochs 50 --gpu=1 \
-      --threshold 0.3 --seed 14 --prefix Apr_05_  --suffix _1 --optimizer adam
-      # --debug  # --stop-early accuracy
-#     --gpu=1  --prefix temp --hidden-size 200 --embedding-size 400 \
-#     --learning-rate 1e-3 --optimizer adam
-# CUDA_VISIBLE_DEVICES=1 python run.py --task track_intent --dataset woz2 \
-#      --model glad --gpu=1 \
-#      --learning-rate 1e-3 --hidden-dim 200 --embedding-size 400 --epochs 30 \
-#      --threshold 0.3 --seed 15 --prefix Apr_04_ --suffix _4 --optimizer adam
+CUDA_VISIBLE_DEVICES=1  python run.py --gpu=1 --task track_intent --dataset woz2 \
+      --model glad --learning-rate 1e-3 --hidden-dim 200 --embedding-size 400 \
+      --epochs 50 --threshold 0.3 --optimizer adam --stop-early success_rate \
+      --prefix Apr_08_  --suffix _1 --seed 14    # --debug
