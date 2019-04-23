bcc71031ef4227bf9deb392505b1bc34a9bbd9cb
Derek Chen <derekchen14@gmail.com>
2019-04-22 17:10:07 -0700

    Update the unit test too, small

--- execute/manage_policy.sh
+++ execute/manage_policy.sh
@@ -2,20 +2,20 @@ CUDA_VISIBLE_DEVICES=0  python run.py --gpu=0 --task manage_policy --model ddq -
     --epsilon 0.0 --hidden-dim 80 --batch-size 16 --seed 14 --warm-start \
     --user simulate --metrics success_rate avg_reward --dataset ddq/movies \
     --learning-rate 1e-3 --optimizer rmsprop --weight-decay 0.0 \
-    --pool-size 5000 --prefix Apr17_ --suffix _unittest --verbose
+    --pool-size 5000 --prefix Apr_22_unittest_ --suffix _14 --verbose
     # --early-stop success_rate --use-existing --report-qual
 
-# python run.py --task manage_policy --model ddq --max-turn 40 -e 500 \
-#     --epsilon 0.0 --hidden-dim 80 --batch-size 16 --seed 15 --warm-start \
-#     --user simulate --metrics success_rate avg_reward --dataset ddq/movies \
-#     --learning-rate 1e-3 --optimizer rmsprop --weight-decay 0.0 \
-#     --pool-size 5000 --prefix Apr_05_unittest_ --suffix _15 --verbose
+CUDA_VISIBLE_DEVICES=0  python run.py --gpu=0 --task manage_policy --model ddq --max-turn 40 -e 500 \
+    --epsilon 0.0 --hidden-dim 80 --batch-size 16 --seed 15 --warm-start \
+    --user simulate --metrics success_rate avg_reward --dataset ddq/movies \
+    --learning-rate 1e-3 --optimizer rmsprop --weight-decay 0.0 \
+    --pool-size 5000 --prefix Apr_22_unittest_ --suffix _15 --verbose
 
-# python run.py --task manage_policy --model ddq --max-turn 40 -e 500 \
-#     --epsilon 0.0 --hidden-dim 80 --batch-size 16 --seed 16 --warm-start \
-#     --user simulate --metrics success_rate avg_reward --dataset ddq/movies \
-#     --learning-rate 1e-3 --optimizer rmsprop --weight-decay 0.0 \
-#     --pool-size 5000 --prefix Apr_05_unittest_ --suffix _16 --verbose
+CUDA_VISIBLE_DEVICES=0  python run.py --gpu=0 --task manage_policy --model ddq --max-turn 40 -e 500 \
+    --epsilon 0.0 --hidden-dim 80 --batch-size 16 --seed 16 --warm-start \
+    --user simulate --metrics success_rate avg_reward --dataset ddq/movies \
+    --learning-rate 1e-3 --optimizer rmsprop --weight-decay 0.0 \
+    --pool-size 5000 --prefix Apr_22_unittest_ --suffix _16 --verbose
 
 
 # python run.py --task policy --model rulebased --report-quant -e 5 \
